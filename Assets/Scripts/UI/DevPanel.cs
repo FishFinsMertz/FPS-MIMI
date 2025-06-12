6d699810abb2f0fa5b4dd2b39993d8ec6c2d790a
FlyingSheep22 <kevinbai222@gmail.com>
2025-06-11 21:33:19 -0400

    InputSystem integration + basic movement

--- Assets/Scripts/UI/DevPanel.cs
+++ Assets/Scripts/UI/DevPanel.cs
@@ -1,22 +1,29 @@
 using TMPro;
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 
 public class DevPanel : MonoBehaviour
 {
     [Header("Necessary References")]
     [SerializeField] private Transform Content;
     [SerializeField] private GameObject ConsoleMessagePrefab;
+    [SerializeField] private InputActionReference toggleKey;
 
     [Header("Specs")]
     [SerializeField] private float consolePaddingRate;
 
     private List<GameObject> messageObjects = new List<GameObject>();
     private List<string> messages = new List<string>();
+    private PlayerInput playerInput;
+    private CanvasGroup canvasGroup;
 
     private void Awake()
     {
         Content = transform.GetChild(0).GetChild(0);
+        playerInput = GetComponent<PlayerInput>();
+        playerInput.actions.FindAction(toggleKey.action.id).performed += ctx => toggleDevPanel();
+        canvasGroup = GetComponent<CanvasGroup>();
     }
 
     void OnEnable()
@@ -48,5 +55,10 @@ public class DevPanel : MonoBehaviour
         msgObj.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, preferredHeight + msgText.textInfo.lineInfo[0].lineHeight * consolePaddingRate);
 
         messages.Add(formattedMessage);
+    }
+    
+    private void toggleDevPanel()
+    {
+        canvasGroup.alpha = canvasGroup.alpha == 0 ? 1 : 0;
     }
 }
